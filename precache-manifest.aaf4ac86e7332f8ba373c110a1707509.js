2f421ef8299f682fa23480f32cf7f68459dc38ef
Sean McCusker <sean.mccusker@gmail.com>
2019-03-08 10:00:08 +1100

    Updates

---
+++ precache-manifest.aaf4ac86e7332f8ba373c110a1707509.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5b881d2e469f5db0c6a",
+    "url": "/syd-beer-client/static/css/main.397aab88.chunk.css"
+  },
+  {
+    "revision": "a5b881d2e469f5db0c6a",
+    "url": "/syd-beer-client/static/js/main.a5b881d2.chunk.js"
+  },
+  {
+    "revision": "ccbde2d716c4a5a13314",
+    "url": "/syd-beer-client/static/js/runtime~main.ccbde2d7.js"
+  },
+  {
+    "revision": "61f548cb49a6f76c1b22",
+    "url": "/syd-beer-client/static/js/2.61f548cb.chunk.js"
+  },
+  {
+    "revision": "596a4a410f686f520644ca420aac0191",
+    "url": "/syd-beer-client/index.html"
+  }
+];
